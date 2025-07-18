e4c3eb12ec7d2221e8b637e884c946237b42e7b9
Ahmar Hayat <ahmar2599@gmail.com>
2025-07-18 22:56:17 +0500

    Fix template error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Hello World")  # or return "Hello, World!" for test
+   return render_template("index.html") # or return "Hello, World!" for test
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Use Railway's port
